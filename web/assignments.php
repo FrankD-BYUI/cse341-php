9166591c2069a13377cb3ec84331ed9ce647d9a2
FrankD-BYUI <fdesjardins@byui.edu>
2021-01-16 01:23:47 -0700

    reverts previous commit and tries again to get heroku working

---
+++ web/assignments.php
@@ -0,0 +1,6 @@
+<?php $pageTitle = "CSE 341 | Assignments";
+include $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'; ?>
+
+<h1>Coming Soon!</h1>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
--- web/prove02/assignments.php
+++
@@ -1,6 +0,0 @@
-<?php $pageTitle = "CSE 341 | Assignments";
-include $_SERVER['DOCUMENT_ROOT'] . '/web/common/header.php'; ?>
-
-<h1>Coming Soon!</h1>
-
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/web/common/footer.php'; ?>
