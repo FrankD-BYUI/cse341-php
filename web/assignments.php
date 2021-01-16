3cc332f9f58f2c193b9689f75f9a02e67b21192c
FrankD-BYUI <fdesjardins@byui.edu>
2021-01-16 01:03:04 -0700

    Adds home page and assignments page

---
+++ web/assignments.php
@@ -0,0 +1,6 @@
+<?php $pageTitle = "CSE 341 | Assignments";
+include $_SERVER['DOCUMENT_ROOT'] . '/web/common/header.php'; ?>
+
+<h1>Coming Soon!</h1>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/web/common/footer.php'; ?>
