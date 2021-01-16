18bb9e0db373aec207654cdd1402b467cb2b59a5
FrankD-BYUI <fdesjardins@byui.edu>
2021-01-16 01:14:16 -0700

    moves files arround to hopefully get it working on heroku

--- web/index.php
+++ web/index.php
@@ -1,13 +1,5 @@
-<?php $pageTitle = "CSE 341 | Desjardins";
-include $_SERVER['DOCUMENT_ROOT'] . '/web/common/header.php'; ?>
+<?php
 
-<h1>Welcome to CSE341!</h1>
+  phpinfo();
 
-<div class="container">
-  <img src="/web/images/frank.jpg" alt="A photo of Frank with child.">
-</div>
-
-<p>This is Frank. He is in his last semester at BYU-I before graduating with a degree in Applied Technology. Frank is only two weeks into the semester, but is already trying to ignore the feeling of fear in the back of his mind telling him that there aren't enough hours in the week to finish all of his homework.</p>
-<p>Frank has been married for three years and has a one year old daughter. Frank enjoys spending time with his family, going on roadtrips, and camping. Between school, work, and <em>*gestures broadly at the world*</em>, 2020 wasn't a good year for some of those activites, but Frank is cautiously optimistic that 2021 will be an improvement.</p>
-
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/web/common/footer.php'; ?>
+?> 
