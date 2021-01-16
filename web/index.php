ac378bea2aead91798371d68ed49dda6ff5973c4
FrankD-BYUI <fdesjardins@byui.edu>
2021-01-16 01:35:42 -0700

    more bug fixes

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'; ?>
 <h1>Welcome to CSE341!</h1>
 
 <div class="container">
-  <img src="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/images/frank.jpg'; ?>" alt="A photo of Frank with child.">
+  <img src="https://i.imgur.com/VqjjENV.jpg" alt="A photo of Frank with child.">
 </div>
 
 <p>This is Frank. He is in his last semester at BYU-I before graduating with a degree in Applied Technology. Frank is only two weeks into the semester, but is already trying to ignore the feeling of fear in the back of his mind telling him that there aren't enough hours in the week to finish all of his homework.</p>
