9ff227fac6c804243f6325ea20d0b07a93122519
FrankD-BYUI <fdesjardins@byui.edu>
2021-01-22 20:12:44 -0700

    readds files eroniously deleted, fleshes out assignments page

--- web/assignments.php
+++ web/assignments.php
@@ -1,6 +1,21 @@
 <?php $pageTitle = "CSE 341 | Assignments";
 include $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'; ?>
 
-<h1>Coming Soon!</h1>
+<h1>Assignments</h1>
+<h2>Week 1:</h2>
+<ul>
+  <li>Prove01: <a href="/hello.html">Hello World</a></li>
+</ul>
+
+<h2>Week 2:</h2>
+<ul>
+  <li>Teach02: <a href="/teach02/index.html">PHP Forms</a></li>
+  <li>Prove02: <a href="/index.php">Homepage</a></li>
+</ul>
+
+<h2>Week 3:</h2>
+<ul>
+  <li><a href="#"></a></li>
+</ul>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
