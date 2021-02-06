e3e7286940c4f0abd978bbc87557307940bace12
FrankD-BYUI <fdesjardins@byui.edu>
2021-02-05 20:40:57 -0700

    attempting to get db working on heroku

--- web/teach05/db.php
+++ web/teach05/db.php
@@ -3,11 +3,6 @@
 function connectDB() {
   $dbUrl = getenv('DATABASE_URL');
 
-  if (empty($dbUrl)) {
-      // example localhost configuration URL with postgres username and a database called cs313db
-      $dbUrl = "postgres://ocwvfhnmjqterq:23fc9e370bfe6d6177eb4c590eced993389439707b5b2a4c0573295d94423a6d@ec2-54-86-189-179.compute-1.amazonaws.com:5432/d92792seehr1o1";
-  }
-
   $dbopts = parse_url($dbUrl);
 
   $dbHost = $dbopts["host"];
