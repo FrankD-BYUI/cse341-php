51099a7b7a43b221a2708c844c1c8a26b13857fe
FrankD-BYUI <fdesjardins@byui.edu>
2021-02-06 11:54:16 -0700

    adds order details to account page

--- db/myDb.sql
+++ db/myDb.sql
@@ -27,14 +27,11 @@ orderItems
 -qty
 */
 
-/*
 DROP TABLE IF EXISTS orderItems;
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS inventory;
 DROP TABLE IF EXISTS users;
-*/
 
-DROP TABLE IF EXISTS inventory;
 CREATE TABLE inventory (
   invID SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR NOT NULL,
@@ -44,7 +41,6 @@ CREATE TABLE inventory (
   qty INT DEFAULT 0
 );
 
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   userID SERIAL NOT NULL PRIMARY KEY,
   fName VARCHAR NOT NULL,
@@ -54,7 +50,6 @@ CREATE TABLE users (
   isAdmin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
   orderID SERIAL NOT NULL PRIMARY KEY,
   userID INT NOT NULL REFERENCES users(userID),
@@ -62,7 +57,6 @@ CREATE TABLE orders (
   orderDate TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-DROP TABLE IF EXISTS orderItems;
 CREATE TABLE orderItems (
   orderID INT NOT NULL REFERENCES orders(orderID),
   invID INT NOT NULL REFERENCES inventory(invID),
@@ -72,16 +66,31 @@ CREATE TABLE orderItems (
 
 INSERT INTO users (fName, lName, email, userPW) VALUES ('Bob', 'Ross', 'Bob@gmail.com', 'H4ppy');
 INSERT INTO users (fName, lName, email, userPW) VALUES ('Harry', 'Potter', 'HPotter@Hogwarts.edu', 'H3dgw1g');
-INSERT INTO inventory (name, description) VALUES ('Wet Dog', 'This candle perfectly captures the aroma of a wet dog.');
-INSERT INTO inventory (name, description) VALUES ('Old Car', 'You know that smell that older cars have? Somewhere between decomposing plastic and long forgotten fries? That''s this candle.');
-INSERT INTO inventory (name, description) VALUES ('Belly Button Funk', 'Have you ever stuck your finger in your belly button and then smelled it? It''s not great, but here it is as a candle.');
-INSERT INTO inventory (name, description) VALUES ('Dumpster Behind the Movie Theater', 'Rancid soda and soggy popcorn, name a more iconic duo.');
+INSERT INTO inventory (name, description, price) VALUES ('Wet Dog', 'This candle perfectly captures the aroma of a wet dog.', 9.99);
+INSERT INTO inventory (name, description, price) VALUES ('Old Car', 'You know that smell that older cars have? Somewhere between decomposing plastic and long forgotten fries? That''s this candle.', 9.99);
+INSERT INTO inventory (name, description, price) VALUES ('Belly Button Funk', 'Have you ever stuck your finger in your belly button and then smelled it? It''s not great, but here it is as a candle.', 9.99);
+INSERT INTO inventory (name, description, price) VALUES ('Dumpster Behind the Movie Theater', 'Rancid soda and soggy popcorn, name a more iconic duo.', 9.99);
+INSERT INTO orders (shippingAddress, userID) VALUES ('12 Privet Dr.', 2);
 INSERT INTO orders (shippingAddress, userID) VALUES ('12 Privet Dr.', 2);
 INSERT INTO orderItems (orderID, invID, qty) VALUES (1, 1, 1);
 INSERT INTO orderItems (orderID, invID, qty) VALUES (1, 4, 2);
+INSERT INTO orderItems (orderID, invID, qty) VALUES (2, 2, 3);
+INSERT INTO orderItems (orderID, invID, qty) VALUES (2, 3, 4);
 
--- SELECT * FROM orders
+
+-- SELECT orders.orderid, orders.shippingaddress, orders.orderdate, orderItems.qty as orderqty,
+--   inventory.invid, inventory.name, inventory.description, inventory.price,
+--   CONCAT(users.fname, ' ', users.lname) as username FROM orders
 --   inner join orderItems on orders.orderid = orderitems.orderid
 --   inner join inventory on orderitems.invid = inventory.invid
 --   inner join users on orders.userid = users.userid
 --   where users.userid = 2;
+
+SELECT orders.orderid, orders.shippingaddress, orders.orderdate,
+  CONCAT(users.fname, ' ', users.lname) as username FROM orders
+  inner join users on orders.userid = users.userid
+  where users.userid = 2;
+
+SELECT inventory.name, orderItems.qty, inventory.price from orderItems
+  inner join inventory on orderitems.invid = inventory.invid
+  WHERE orderItems.orderid = 1;
--- web/prove05/account.php
+++ web/prove05/account.php
@@ -11,16 +11,74 @@ $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 $stmt->closeCursor();
 
+$sql = 'SELECT orders.orderid, orders.shippingaddress, orders.orderdate,
+            CONCAT(user.fname, " ", users.lname) as username 
+          FROM orders
+          inner join users on order.userid = users.userid
+          WHERE users.userid = :userid';
+$stmt = $connection->prepare($sql);
+$stmt->bindValue(':userid', $userid, PDO::PARAM_INT);
+$stmt->execute();
+$orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt->closeCursor();
 
+function getOrderDetails($ordernum)
+{
+  $connection = connectDB();
+  $sql = 'SELECT inventory.name, orderItems.qty, inventory.price 
+            FROM orderItems
+            inner join inventory on orderitems.invid = inventory.invid
+            WHERE orderItems.orderid = :ordernum';
+  $stmt = $connection->prepare($sql);
+  $stmt->bindValue(':ordernum', $ordernum, PDO::PARAM_INT);
+  $stmt->execute();
+  $orderDetails = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $stmt->closeCursor();
+  return $orderDetails;
+}
 
 $pageTitle = "Silly Scents | My Account";
 include $_SERVER['DOCUMENT_ROOT'] . '/prove05/common/header.php';
 ?>
 
-<h1>Hello<?php echo $user['fname']; ?></h1>
+<h1>Hello <?php echo $user['fname']; ?></h1>
 <p>Name: <?php echo $user['fname'] . " " . $user['lname'] ?></p>
-<p>Email: <?php echo $user['email']?> </p>
+<p>Email: <?php echo $user['email'] ?> </p>
 
 <h2>Order History</h2>
+<?php
+if ($orders) {
+  echo '<table>
+    <tr>
+      <th>Order Number:</th>
+      <th>Order Date:</th>
+      <th>Items:</th>
+      <th>Price:</th>
+      <th>Shipping Address</th>
+    </tr>';
+
+  foreach ($orders as $order) {
+    $orderDetails = getOrderDetails($order);
+    $orderItems = 0;
+    $orderPrice = 0.00;
+
+    foreach ($orderDetails as $item) {
+      $orderItems += $item['qty'];
+      $orderPrice += $item['price'];
+    }
+
+    echo "<tr>
+        <td>$order[orderid]</td>
+        <td>date('m/d/Y',$order[orderdate])</td>
+        <td>$orderItems</td>
+        <td>$orderPrice</td>
+        <td>
+          <div>$order[username]</div>
+          <div>$order[shippingaddress]</div>
+        </td>
+      </tr>";
+  }
+  echo '</table>';
+} ?>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . 'prove05/common/footer.php'; ?>
