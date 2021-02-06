ebc86981beb6100498437855f2891a18c93fda94
FrankD-BYUI <fdesjardins@byui.edu>
2021-02-05 22:44:13 -0700

    maybe?

--- web/teach05/db.php
+++ web/teach05/db.php
@@ -1,31 +1,27 @@
 <?php
 
 function connectDB() {
-  $dbUrl = getenv('DATABASE_URL');
+    $dbUrl = getenv('DATABASE_URL');
 
-  $dbopts = parse_url($dbUrl);
-  var_dump($dbopts);
+    $dbopts = parse_url($dbUrl);
+    var_dump($dbopts);
 
-  $dbHost = $dbopts["host"];
-  $dbPort = $dbopts["port"];
-  $dbUser = $dbopts["user"];
-  $dbPassword = $dbopts["pass"];
-  $dbName = ltrim($dbopts["path"],'/');
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
 
-  $dsn = "pgsql:host=$dbHost;port=$dbPort;dbname=$dbName";
-  $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-  // Create the actual connection object and assign it to a variable
-  try {
-      $link = new PDO($dsn, $dbUser, $dbPassword, $options);
-      if(is_object($link)) {
-          return $link;
-      }
-  } catch(PDOException $e) {
-      var_dump($e);
-      exit;
-  }
+    $dsn = "pgsql:host=$dbHost;port=$dbPort;dbname=$dbName";
+    //$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    // Create the actual connection object and assign it to a variable
+    try {
+        $link = new PDO($dsn, $dbUser, $dbPassword);
+        $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $link;
+    } catch(PDOException $e) {
+        var_dump($e);
+        echo "it failed!";
+        exit;
+    }
 }
-
-
-
-?>
