1b86fbdf7c033242831227ed9ebbf4cb1b315f1e
FrankD-BYUI <fdesjardins@byui.edu>
2021-02-05 22:09:27 -0700

    best

--- web/teach05/index.php
+++ web/teach05/index.php
@@ -15,17 +15,17 @@ if ($search) {
   $sql = "SELECT * from scriptures WHERE book = :search";
 }
 
-// $stmt = $connection->prepare($sql);
+$stmt = $connection->prepare($sql);
 
-// $stmt->bindValue(':search', $search, PDO::PARAM_STR);
-// $stmt->execute();
-// $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// $stmt->closeCursor();
+$stmt->bindValue(':search', $search, PDO::PARAM_STR);
+$stmt->execute();
+$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt->closeCursor();
 
-// $message = "";
-// if ($stmt->rowCount() == 0) {
-//   $message = "No results found.";
-// }
+$message = "";
+if ($stmt->rowCount() == 0) {
+  $message = "No results found.";
+}
 
 
 ?>
